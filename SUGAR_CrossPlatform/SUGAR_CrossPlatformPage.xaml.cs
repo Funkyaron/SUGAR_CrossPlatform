65d615ce0fe305a3cf11dd61ef990282cc582f05
shk <aron.proebsting@student.uni-siegen.de>
2018-02-23 10:33:25 +0100

    Initial Commit

---
+++ SUGAR_CrossPlatform/SUGAR_CrossPlatformPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SUGAR_CrossPlatform
+{
+    public partial class SUGAR_CrossPlatformPage : ContentPage
+    {
+        public SUGAR_CrossPlatformPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
