cbd01fd4d6bd7c7f54c30bbba6a8c7bbb922275d
shk <aron.proebsting@student.uni-siegen.de>
2018-04-06 10:52:42 +0200

    Finished contacts stuff.

---
+++ Droid/ContactsFetcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SUGAR_CrossPlatform.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(ContactsFetcher))]
+namespace SUGAR_CrossPlatform.Droid
+{
+    public class ContactsFetcher : IContactsFetcher
+    {
+        public ContactsFetcher()
+        {
+        }
+
+        public SUGARContact[] GetAllContacts() {
+            return new SUGARContact[0];
+        }
+    }
+}
--- SUGAR_CrossPlatform/TestPage.xaml.cs
+++ SUGAR_CrossPlatform/TestPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 using Xamarin.Forms;
 
@@ -8,98 +9,68 @@ namespace SUGAR_CrossPlatform
 {
     public partial class TestPage : ContentPage
     {
+        private const string privateNumber = "0163 9018985";
+
         public TestPage()
         {
             InitializeComponent();
         }
 
-        public void SaveTestProfile(object sender, EventArgs e) {
-            Profile prof = new Profile();
-            prof.Name = "Kurz";
-            prof.PhoneNumbersAsStrings.Add("1234");
-            prof.PhoneNumbersAsStrings.Add("5432");
-            prof.PhoneNumbersAsLongs.Add(1234);
-            prof.PhoneNumbersAsLongs.Add(5678);
-            prof.ContactNames.Add("Marius Müller");
-            prof.ContactNames.Add("Max Mustermann");
-            prof.ContactNames.Add("Martin Mikus");
+        public void FetchContacts(object sender, EventArgs args) {
+            IContactsFetcher fetcher = DependencyService.Get<IContactsFetcher>();
+            SUGARContact[] allContacts = fetcher.GetAllContacts();
 
-            TimeUnit[] startTimes = prof.StartTimes;
-            startTimes[4] = new TimeUnit(7, 0);
-            prof.StartTimes = startTimes;
-            TimeUnit[] endTimes = prof.EndTimes;
-            endTimes[4] = new TimeUnit(13, 0);
-            prof.EndTimes = endTimes;
-            bool[] days = prof.Days;
-            days[4] = true;
-            prof.Days = days;
-            prof.Active = true;
-            prof.Allowed = false;
-
-            ProfileManager mgr = new ProfileManager();
-            bool success = mgr.SaveProfile(prof);
-            if(success) {
-                ResultLabel.Text = "Profile saved.";
-            } else {
-                ResultLabel.Text = "Error while saving.";
+            ResultLabel.Text = "";
+            foreach(var contact in allContacts) {
+                ResultLabel.Text += $"{contact}\n";
             }
         }
 
-        public void ReadAsText(object sender, EventArgs e) {
-            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            var filePath = Path.Combine(documentsPath, "Kurz.xml");
+        public void RequestAccess(object sender, EventArgs args) {
             try {
-                ResultLabel.Text = File.ReadAllText(filePath);
-            } catch(Exception ex) {
-                ResultLabel.Text = ex.StackTrace;
-            }
-        }
-
-        public void ReadAsProfile(object sender, EventArgs e) {
-            ProfileManager mgr = new ProfileManager();
-            Profile prof = mgr.GetProfile("Kurz");
-            if (prof != null) {
-                ResultLabel.Text = prof.ToString();
-            } else {
-                ResultLabel.Text = "Error while reading.";
-            }
-        }
-
-        public void InitProfile(object sender, EventArgs args) {
-            try {
-                ProfileManager mgr = new ProfileManager();
-                Profile prof = mgr.GetProfile("Kurz");
-                if(prof != null) {
-                    mgr.InitProfile(prof);
-                    ResultLabel.Text = "Profile initialized";
-                } else {
-                    ResultLabel.Text = "Error while reading";
-                }
+                //new CNContactStore().RequestAccess(CNEntityType.Contacts, (granted, error) => { });
             } catch(Exception e) {
-                ResultLabel.Text = $"Error while initializing: {e}";
+                ResultLabel.Text = e.ToString();
             }
-
         }
 
-        public void Compare(object sender, EventArgs args) {
-            TimeUnit tu1 = new TimeUnit(11, 0);
-            TimeUnit tu2 = new TimeUnit(10, 0);
-            bool earlier = tu1 < tu2;
-            bool earlierOrEqual = tu1 <= tu2;
+        public void ShowNumber(object sender, EventArgs args) {
+            ResultLabel.Text = privateNumber;
+        }
 
-            string resultText = $"Time 1: {tu1}\nTime 2: {tu2}\n";
-            if(earlier) {
-                resultText += "Time 1 is earlier than time 2\n";
-            } else {
-                resultText += "Time 1 is later or equal time 2\n";
-            }
-            if(earlierOrEqual) {
-                resultText += "Time 1 is earlier or equal time 2";
-            } else {
-                resultText += "Time 1 is later than time 2";
-            }
+        public void ShowNormalized(object sender, EventArgs args) {
+            IContactsFetcher con = DependencyService.Get<IContactsFetcher>();
+            //ResultLabel.Text = con.NormalizeNumber(privateNumber);
+        }
 
-            ResultLabel.Text = resultText;
+        public void DisplayCountryCode(object sender, EventArgs args) {
+            IContactsFetcher con = DependencyService.Get<IContactsFetcher>();
+            //ResultLabel.Text = con.GetCountryCode();
+        }
+
+        public void ShowLong(object sender, EventArgs args) {
+            IContactsFetcher contacts = DependencyService.Get<IContactsFetcher>();
+            /*string normalizedNumber = contacts.NormalizeNumber(privateNumber);
+            long longNumber = 0;
+            try {
+                longNumber = ParseNumberAsLong(normalizedNumber);
+                ResultLabel.Text = longNumber.ToString();
+            } catch(Exception e) {
+                string bla = e.StackTrace;
+                longNumber = 123;
+                ResultLabel.Text = normalizedNumber;
+            }*/
+        }
+
+        public long ParseNumberAsLong(string originalNumber) {
+            StringBuilder builder = new StringBuilder();
+            foreach (char c in originalNumber) {
+                if (char.IsDigit(c)) {
+                    builder.Append(c);
+                }
+            }
+            string resultString = builder.ToString();
+            return Int64.Parse(resultString);
         }
     }
 }
