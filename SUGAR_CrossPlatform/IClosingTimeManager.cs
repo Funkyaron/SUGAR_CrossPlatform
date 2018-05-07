9f34d57b47ce59d1f7400e588855dd30aa61c588
shk <aron.proebsting@student.uni-siegen.de>
2018-05-07 08:10:59 +0200

    Fixed ProfileManager bug

---
+++ SUGAR_CrossPlatform/IClosingTimeManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SUGAR_CrossPlatform
+{
+    public interface IClosingTimeManager
+    {
+        void SaveAndScheduleClosingTime(DayOfWeek dayOfWeek, TimeUnit time);
+        void RemoveAndCancelClosingTime(DayOfWeek dayOfWeek);
+        TimeUnit[] GetAllClosingTimes();
+    }
+}
